79a4a80f8690efc3a1061e62c85c0073e1a89723
Abhya1559 <abhya1559@gmail.com>
2025-07-11 18:46:41 +0530

    feat: implement login functionality with user authentication and error handling

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse, NextRequest } from "next/server";
+import { initDB } from "@/lib/init-db";
+import { User } from "@/models/User";
+import bcrypt from "bcrypt";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { email, password } = body;
+
+    if (!email || !password) {
+      return NextResponse.json({ error: "all fields are required" });
+    }
+    const db = await initDB();
+    const userRepo = db.getRepository(User);
+    const user = await userRepo.findOneBy({ email });
+
+    if (!user) {
+      return NextResponse.json({
+        error: "user not exist in data please register ",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return NextResponse.json({ error: "credential error" }, { status: 402 });
+    }
+
+    return NextResponse.json({
+      message: "user logged in successful",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      status: 200,
+    });
+  } catch (error) {
+    console.error("server error while login", error);
+    return NextResponse.json(
+      { error: "Server error while login" },
+      { status: 500 }
+    );
+  }
+}
