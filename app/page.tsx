33e53d79a5312dabe5cee3be01e10c8642597f01
Abhya1559 <abhya1559@gmail.com>
2025-07-13 20:22:45 +0530

    added db url and site layout changes and color added

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="mt-10 flex flex-col min-h-screen px-6 sm:px-20 pt-10 pb-0 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex-grow flex flex-col items-center space-y-24">
+    <div className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="mt-20 flex-grow flex flex-col max-w-[1320px] mx-auto items-center space-y-24">
         <Hero />
         <Features />
       </main>
--- lib/data-source.ts
+++ lib/data-source.ts
@@ -9,12 +9,8 @@ import { Queue } from "@/models/Queue";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "3306", 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: "postgres",
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
   entities: [User, Doctor, Appointment, Queue],
